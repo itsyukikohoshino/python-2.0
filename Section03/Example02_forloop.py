5895538e5f1f2cef882289f6b2b65ebce3f06931
itsyukikohoshino <yukikohoshinostudio@gmail.com>
2024-01-05 15:09:22 -0600

    forloop

---
+++ Section03/Example02_forloop.py
@@ -0,0 +1,24 @@
+
+
+numberArray = [1,2,3,4,5 ]
+
+stringArray = ["lego","barbie","cars"]
+
+
+for x in stringArray:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+print (x)
+
+

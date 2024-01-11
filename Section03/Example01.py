d4aa98881128b27b30fd48440f2678a0d27c24de
itsyukikohoshino <yukikohoshinostudio@gmail.com>
2024-01-11 11:12:53 -0600

    u

---
+++ Section03/Example01.py
@@ -0,0 +1,12 @@
+x = 42
+y = 3 / 4
+z = int('7')
+a = float(5)
+name = "Nina"
+
+rent = 480
+per_day = rent / 30
+print (per_day)
+16.0
+
+print("hello world")
--- Section03/Example02_forloop.py
+++ Section03/Example02_forloop.py
@@ -1,24 +1,54 @@
 
 
-numberArray = [1,2,3,4,5 ]
+# numberArray = [1,2,3,4,5 ]
 
-stringArray = ["lego","barbie","cars"]
+# stringArray = ["lego","barbie","cars"]
 
 
-for x in stringArray:
-    print(x)
-
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-    print(x)
-    if x == "banana":
-        break
+# for x in stringArray:
+#     print(x)
 
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
     print(x)
     if x == "banana":
         break
-print (x)
+ 
 
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     print("1x: ", x)
+#     if x == "banana":
+#         break
+# print ("2x: ", x)
 
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     print(x)
+#     if x == "banana":
+#         continue
+#     print ("fruits: ", x)
+
+# for x in range(6):
+#     print(x)
+  
+
+ 
+ 
+for i in range(1, 11):
+ 
+    # If i is equals to 6,
+    # continue to next iteration
+    # without printing
+    if i == 6:
+        continue
+    else:
+        # otherwise print the value
+        # of i
+        print(i, end=" ")
